16592cfa0297b5886e363bc269fe0a482e38363a
Andeline <andelineya@gmail.com>
2021-05-11 19:01:57 +0800

    Initial Commit

---
+++ 數學小神童/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  數學小神童
+//
+//  Created by 蔡佳穎 on 2021/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
